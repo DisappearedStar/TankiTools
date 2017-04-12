b750e123b1511fa2d5bc0e9f7ab0e32fcae8f349
Dmitry <dont.regret1@gmail.com>
2017-04-12 19:20:56 +0700

    добавлено CheckPorts

---
+++ TankiTools/Network.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace TankiTools
+{
+    public class TankiPort
+    {
+        public TankiPort(int p)
+        {
+            port = p;
+            isOpen = false;
+        }
+        public int port { get; set; }
+        public bool isOpen { get; set; }
+
+    }
+
+    class Network
+    {
+
+
+        private string GetIpAddress()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return webClient.DownloadString("https://api.ipify.org");
+            }
+        }
+
+        private List<TankiPort> CheckPorts(int[] portsArray)
+        {
+            var ports = new List<TankiPort>();
+            for(int i = 0; i < portsArray.Length; i++)
+            {
+                ports.Add(new TankiPort(portsArray[i]));
+            }
+            string ip = ResolveHostToIp("http://c1.eu.tankionline.com/");
+
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                for(int i = 0; i < ports.Count; i++)
+                {
+                    try
+                    {
+                        tcpClient.Connect(ip, ports[i].port);
+                        ports[i].isOpen = true;
+                    }
+                    catch (Exception) { }
+                }
+            }
+            return ports;
+        }
+
+
+        private string ResolveHostToIp(string host)
+        {
+            return Dns.GetHostEntry(host).AddressList[0].ToString();
+        }
+    }
+}
